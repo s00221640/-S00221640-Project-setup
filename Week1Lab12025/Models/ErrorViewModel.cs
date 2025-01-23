8a3d7d5e7dfbda437b468e05f2d3b9a8da4fb5b1
S00221640 <S00221640@atu.ie>
2025-01-23 10:30:09 +0000

    First commit

---
+++ Week1Lab12025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Week1Lab12025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Week1Lab12025/Program.cs
@@ -0,0 +1,43 @@
+using Tracker.WebAPIClient;
+
+namespace Week1Lab12025
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+
+            ActivityAPIClient.Track(StudentID: "s00221640", StudentName: "Jack Tierney",
+            activityName: "Rad302 2025 Week 1 Lab 1", Task: " Project Setup ");
+
+        }
+    }
+}
+
