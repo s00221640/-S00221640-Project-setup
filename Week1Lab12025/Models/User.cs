3f4a1281df8374d7fd5b48f8f227be3f9133feab
S00221640 <S00221640@atu.ie>
2025-01-23 10:45:29 +0000

    Step 6

---
+++ Week1Lab12025/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Week1Lab12025.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public int MidName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        
+
+    }
+}
