213703b7e63da28b61c745c97f60128b439a14d9
S00221640 <S00221640@atu.ie>
2025-01-23 10:51:38 +0000

    Step 7

---
+++ Week1Lab12025/Models/UserContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Week1Lab12025.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext()
+            : base()
+        {
+            //Database.EnsureCreated(); // Automatic migrations
+        }
+
+        public UserContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+    }
+}
